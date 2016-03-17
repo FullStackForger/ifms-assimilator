3b916664e7f69f3ca8e3e4064c8d27c754de54ff
IndieForger <indieforger@gmail.com>
2016-03-17 16:11:16 +0000

    indieforger and fullstackforger are now separate blogs

---
+++ config/fullstackforger.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+	server: {
+		env: "production",
+		host: "localhost",
+		port: 8003
+	},
+	settings: {
+		globals: {
+			path: __dirname + '/..'
+		},
+		theme: {
+			path: './themes/assimilator',
+			layout: 'default'
+		},
+		blog: {
+			path: '../blog.fullstackforger.com'
+		},
+		pages: {
+			path: './pages/'
+		},
+		files: {
+			index: ['index.html', 'index.htm'],
+			path: './projects/'
+		}
+	},
+	context: {
+		title: 'My personal site',
+		menu: {
+			main: [
+				{ label: 'about', url: '/about' },
+				{ label: 'contact', url: '/contact' },
+				{ label: 'project-demo', url: '/project-demo' }
+			]
+		},
+		// font awesome based icons
+		social: [
+			{ channel: 'twitter', url: 'https://twitter.com/fullstackforger' },
+			{ channel: 'gamepad', url: 'https://indieforger.com' }
+		]
+	}
+}
