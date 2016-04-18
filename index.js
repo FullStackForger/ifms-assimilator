44a849fafcbd733ac09971df21b820a92bd8891e
IndieForger <indieforger@gmail.com>
2016-04-18 13:06:54 +0100

    config moved into ./config folder for easier deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Assimilator = require('assimilator');
 
 Assimilator
-	.start(require('./config.js'))
+	.start(require('./config/config.js'))
 	.then((server) => {
 		server.connections.forEach((connection) => {
 			console.log(`\nServer started at: ${connection.info.uri}`);
