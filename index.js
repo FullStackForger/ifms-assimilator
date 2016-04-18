637cb4aae3aa34be998a0e268a759300dd1e3f97
IndieForger <indieforger@gmail.com>
2016-04-18 13:01:32 +0100

    update to assimilator 0.5.0, fixes #1

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-'use strict'
-const Assimilator = require('assimilator')
-const Forger = require('forger')
+'use strict';
+const Assimilator = require('assimilator');
 
-const fullstackConfig = require('./config/fullstackforger.config')
-const indieConfig = require('./config/indieforger.config')
-
-function startServer(config, next) {
-	new Assimilator
-		.Server(config)
-		.start().then(() => next())
-		.catch((err) => next(err))
-}
-
-Forger
-	.sequence(
-		(next) => startServer(fullstackConfig, next),
-		(next) => startServer(indieConfig, next)
-	)
-	.then(() => console.log('All servers started'))
-	.catch((error) => console.error(error))
+Assimilator
+	.start(require('./config.js'))
+	.then((server) => {
+		server.connections.forEach((connection) => {
+			console.log(`\nServer started at: ${connection.info.uri}`);
+		});
+	})
+	.catch((error) => {
+		console.error(error.stack);
+	});
